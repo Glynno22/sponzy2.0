167f923429fca161618bc7d701e1f50bac7499e5
Glynno22 <donaldnoukimi@gmail.com>
2024-07-02 12:02:05 +0100

    Debutprojet 2.0

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+    </head>
+    <body class="antialiased">
+        <div class="">
+           <p class="text-red-500">hello</p>
+        </div>
+    </body>
+</html>
